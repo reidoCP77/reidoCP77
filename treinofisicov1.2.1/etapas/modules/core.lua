ec5f10b98ed93e9b03ad227807041d240685b2ae
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2026-01-02 01:16:54 -0300

    Atualizar o core.lua

--- treinofisicov1.2.1/etapas/modules/core.lua
+++ treinofisicov1.2.1/etapas/modules/core.lua
@@ -6,3 +6,7 @@ local Gui = Instance.new("ScreenGui", plrGui")
 local Frame = Instance.new("Frame", Gui)
 Frame.Position = UDim2.new(0.45,0,0.5,0)
 Frame.Size = UDim2.new(0.45, 0, 0.46,0)
+Frame.BackgroundColor3 = Color3.fromRGB(60,60,60)
+
+local uiCorner = Instance.new("UICorner", Frame)
+uiCorner.CornerRadius = UDim.new(0,12)
