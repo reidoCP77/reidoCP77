1273f976cfd4789004dd203fff7346b02fed1bae
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2026-01-01 21:22:16 -0300

    Atualizar o UI.lua

--- treinofisicov1.2.1/UI.lua
+++ treinofisicov1.2.1/UI.lua
@@ -3,6 +3,6 @@ local port = ui:mainport(3000)
 local stats = ui:stats(0)
 local data = ui:data()
 local server = ui:server(data, port)
-while stats == 0 then
+while stats < 0 then
    ui:RunServer(server)
 end
