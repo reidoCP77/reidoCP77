9ed678ddd96d34a38c440c2b7e179ee28583f640
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2026-01-02 10:08:10 -0300

    Atualizar o core.lua

--- treinofisicov1.2.1/etapas/modules/core.lua
+++ treinofisicov1.2.1/etapas/modules/core.lua
@@ -9,8 +9,8 @@ local Languages = {
 local Gui = Instance.new("ScreenGui", plrGui)
 
 local Frame = Instance.new("Frame", Gui)
-Frame.Position = UDim2.new(0.4,0,-0.1,0)
-Frame.Size = UDim2.new(0.5, 0, 0.7,0)
+Frame.Position = UDim2.new(0.25,0,-0.1,0)
+Frame.Size = UDim2.new(0.5, 0, 0.9,0)
 Frame.BackgroundColor3 = Color3.fromRGB(60,60,60)
 Frame.Draggable = true
 
@@ -21,21 +21,25 @@ local uiStroke = Instance.new("UIStroke", Frame)
 
 local frameName = Instance.new("TextLabel", Frame)
 
-frameName.Position = UDim2.new(0.1,0,-.05,0)
-frameName.Font = Enum.Font.GothamBold
 frameName.TextScaled = true
+frameName.Position = UDim2.new(.041,0,-.15,0)
+frameName.Font = Enum.Font.GothamBold
 frameName.BackgroundTransparency = 1
 frameName.TextColor3 = Color3.fromRGB(230,230,230)
-frameName.Text = "Core-Gui"
+frameName.Text = "Treinamento"
+frameName.Size = UDim2.new(0.5,0,0.5,0)
 
 local UIC = Instance.new("UICorner")
 UIC.CornerRadius = UDim.new(0,6)
-local menu = Instance.new("Frame", Frame)
-menu.Position = UDim2.new(0.03,0,0.2,0)
-menu.Size = UDim2.new(0.85,0,.6,0)
+local menu = Instance.new("ScrollingFrame", Frame)
+
+menu.Position = UDim2.new(0.045,0,0.2,0)
+menu.Size = UDim2.new(0.9,0,.78,0)
 menu.BackgroundColor3 = Color3.fromRGB(80,80,80)
+UIC.Parent = menu
 
 local min = Instance.new("TextButton", Gui)
+
 min.Visible = false
 local UIC3 = Instance.new("UICorner", min)
 min.Text = "Abrir"
@@ -43,21 +47,22 @@ min.TextScaled = true
 min.BackgroundTransparency = .8
 min.TextColor3 = Color3.fromRGB(240,240,240)
 
-min.MouseButton1Click:Connect(function(
+min.MouseButton1Click:Connect(function()
    Frame.Visible = true
-))
+end)
 
-local UIS = game.UserInputService
 local fechar = Instance.new("TextButton", Frame)
+
 fechar.Text = "X"
-fechar.TextScaled = true
+fechar.Position = UDim2.new(.85,0,0.03,0)
 local UIC2 = Instance.new("UICorner", fechar)
 UIC2.CornerRadius = UDim.new(0,99)
-fechar.BackgroundColor3 = Color3.fromRGB(130,130,130)
+fechar.BackgroundColor3 = Color3.fromRGB(255,130,130)
 fechar.TextColor3 = Color3.fromRGB(230,230,230)
+fechar.Size = UDim2.new(0.1,0,0.13,0)
 
-fechar.MouseButton1Click:Connect(function(
+fechar.MouseButton1Click:Connect(function()
     Frame.Visible = false
-))
+end
 
-
+local UISt2 = Instance.new("UIStroke", menu)
