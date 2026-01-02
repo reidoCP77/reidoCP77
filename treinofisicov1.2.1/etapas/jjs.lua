1b4d5ca526e96cf87ff776fc585b40b075367bc1
Gabriel da Silva Gomes <gabri2012mi@gmail.com>
2026-01-01 21:02:28 -0300

    Atualizar o jjs.lua

--- treinofisicov1.2.1/etapas/jjs.lua
+++ treinofisicov1.2.1/etapas/jjs.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("url"))()
+local Options = { Keybind = 'Home', --> Keybind para mostrar/esconder a UI, mais informações sobre KeyCode: https://create.roblox.com/docs/reference/engine/enums/KeyCode Tempo = 2.5, --> Tempo para enviar mensagem. Rainbow = false, --> Deixar a UI mais colorida (true/false) Language = { UI = 'pt-br', --> Alterar a linguagem da UI, disponívels: pt-br, en-us Words = 'pt-br' --> Alterar a linguagem dos número em extenso, disponívels: pt-br, en-us, es-es }, };
